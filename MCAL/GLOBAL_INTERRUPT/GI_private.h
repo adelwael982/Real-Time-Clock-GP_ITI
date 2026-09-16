642952720f9ae0a50d0aeb5d85eabec3cd7cbcac
Adel Wael <adelwael2006@gmail.com>
2026-09-17 02:36:10 +0300

    Add global interrupt registers

---
+++ MCAL/GLOBAL_INTERRUPT/GI_private.h
@@ -0,0 +1,15 @@
+/*
+
+* GI_private.h
+ *
+ *  Created on: Sep 2, 2026
+ *      Author: mosta
+ */
+
+#ifndef MCAL_GLOBAL_INTERRUPT_GI_PRIVATE_H_
+#define MCAL_GLOBAL_INTERRUPT_GI_PRIVATE_H_
+
+#define   SREG 				*((volatile u8*)0x5F)
+#define SREG_I         7
+
+#endif /* MCAL_GLOBAL_INTERRUPT_GI_PRIVATE_H_ */
