c00a26695aedc5ee8e901655e7d7b61f404fcca1
Adel Wael <adelwael2006@gmail.com>
2026-09-17 02:34:53 +0300

    Add RTC standard types

---
+++ STD_TYPES.h
@@ -0,0 +1,30 @@
+/*
+ * STD_TYPES.h
+ *
+ *  Created on: Aug 26, 2026
+ *      Author: pc
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+#define STD_TYPES_OK       1
+#define STD_TYPES_NOK      0
+
+
+typedef unsigned char           u8;
+typedef unsigned short int      u16;
+typedef unsigned long int       u32;
+typedef unsigned long long int  u64;
+typedef signed char             s8;
+typedef signed short int        s16;
+typedef signed long int         s32;
+typedef float                   f32;
+typedef double                  f64;
+typedef long double             f128;
+
+
+#define NULL ((void*)0)
+
+#endif /* STD_TYPES_H_ */
