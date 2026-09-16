1abdfc67abddab2b6d28e869cd3c3b23275233be
Adel Wael <adelwael2006@gmail.com>
2026-09-17 02:36:05 +0300

    Add global interrupt interface

---
+++ MCAL/GLOBAL_INTERRUPT/GI_interface.h
@@ -0,0 +1,14 @@
+/*
+ * GI_interface.h
+ *
+ *  Created on: Sep 2, 2026
+ *      Author: mosta
+ */
+
+#ifndef MCAL_GLOBAL_INTERRUPT_GI_INTERFACE_H_
+#define MCAL_GLOBAL_INTERRUPT_GI_INTERFACE_H_
+
+void GI_Enable(void);
+void GI_Disable(void);
+
+#endif /* MCAL_GLOBAL_INTERRUPT_GI_INTERFACE_H_ */
