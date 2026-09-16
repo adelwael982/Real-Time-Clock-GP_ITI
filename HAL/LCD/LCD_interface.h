a1c24a9f8dfd462ad0d232afabcfc2c9a054815c
Adel Wael <adelwael2006@gmail.com>
2026-09-17 02:35:41 +0300

    Add LCD interface

---
+++ HAL/LCD/LCD_interface.h
@@ -0,0 +1,31 @@
+/*
+
+* LCD_interface.h
+ *
+ *  Created on: Aug 27, 2026
+ *      Author: mosta
+ */
+
+#ifndef HAL_LCD_LCD_INTERFACE_H_
+#define HAL_LCD_LCD_INTERFACE_H_
+
+#define LCD_8_BIT_MODE		1
+#define LCD_4_BIT_MODE 		2
+
+#define LCD_LINE_ONE		1
+#define LCD_LINE_TWO		2
+
+#define LCD_SHIFT_LEFT 		0
+#define LCD_SHIFT_RIGHT 	1
+
+void LCD_init(void);
+void LCD_sendcmnd( u8 cmnd);
+void LCD_sendChar (u8 data);
+void writeHalfPort(u8 Value);
+void LCD_writeString(u8 * string);
+void LCD_writeNumber( u32 number);
+void LCD_clear(void);
+void LCD_shift(u8 shiftingdirection);
+void LCD_goToSpecificPosition (u8 LineNumber , u8 Position);
+
+#endif /* HAL_LCD_LCD_INTERFACE_H_ */
