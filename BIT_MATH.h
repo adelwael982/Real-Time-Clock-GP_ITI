60f8ff4dd0fef111cc9d8e3c5d82dc7215901efb
Adel Wael <adelwael2006@gmail.com>
2026-09-17 02:34:49 +0300

    Add RTC project standard headers

---
+++ BIT_MATH.h
@@ -0,0 +1,20 @@
+/*
+ * Maths_bits.h
+ *
+ *  Created on: Aug 26, 2026
+ *      Author: pc
+ */
+
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+
+
+#define SET_BIT(REG,BIT_NUM)  (REG|=(1<<BIT_NUM))
+#define CLR_BIT(REG,BIT_NUM)  (REG&=(~(1<<BIT_NUM)))
+#define TOG_BIT(REG,BIT_NUM)  (REG^=(1<<BIT_NUM))
+#define GET_BIT(REG,BIT_NUM)  ((REG>>BIT_NUM)&1)
+
+
+#endif /* BIT_MATH_H_ */
